e7126c954afcfe082dc9116bddbafc4f586aa837
Jason Chiang <chiangcy918@gmail.com>
2022-03-02 14:36:44 +0800

    pod install

---
+++ Pods/Target Support Files/Pods-Gogolooktest-GogolooktestUITests/Pods-Gogolooktest-GogolooktestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Gogolooktest_GogolooktestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Gogolooktest_GogolooktestUITests
+@end
---
+++ Pods/Target Support Files/Pods-Gogolooktest/Pods-Gogolooktest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Gogolooktest : NSObject
+@end
+@implementation PodsDummy_Pods_Gogolooktest
+@end
